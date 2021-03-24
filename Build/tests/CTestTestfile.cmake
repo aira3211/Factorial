7973318122ec261aeb6fbe4bf6a356f5ca74462d
aria <aryashahbazi@gmail.com>
2021-03-25 01:50:09 +0430

    Fac
    Done cmake

---
+++ Build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/Factorial/Factorial/tests
+# Build directory: G:/C++/AW/Factorial/Factorial/Build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(myLibrary.unit_tests "G:/C++/AW/Factorial/Factorial/Build/unit_tests.exe")
+set_tests_properties(myLibrary.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "G:/C++/AW/Factorial/Factorial/tests/CMakeLists.txt;22;add_test;G:/C++/AW/Factorial/Factorial/tests/CMakeLists.txt;0;")
--- app/main.cpp
+++ app/main.cpp
@@ -19,7 +19,6 @@ int main()
 
 int Factorial(int n)
 {
-  int fac = 1;
   if (n==0 || n==1)
   {
     return 1;
