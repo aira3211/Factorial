dff8c2a399492d5eeef88b3b5f758213b5cf7f03
aria <aryashahbazi@gmail.com>
2021-03-25 01:46:30 +0430

    Factorial

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,34 @@
-
 #include <iostream>
-#include <stdlib.h>
 
-#include "Foo.h"
+using namespace std;
+
+
+
+int Factorial(int n);
+
 int main()
 {
-  Foo d = Foo();
-  std::cout<< d.doSomething()<<std::endl;
+  int n  = 10;
+  int m  = 9;
+  int x = 6;
+  cout << n << "!" << " = " << Factorial(n)<<endl;
+  cout << m << "!" << " = " << Factorial(m)<<endl;
+  cout << x << "!" << " = " << Factorial(x)<< endl;
 
-  return 0;
+}
+
+int Factorial(int n)
+{
+  int fac = 1;
+  if (n==0 || n==1)
+  {
+    return 1;
+  }
+  else
+
+  return (Factorial(n-1)*n);
+
+
+
+
 }
